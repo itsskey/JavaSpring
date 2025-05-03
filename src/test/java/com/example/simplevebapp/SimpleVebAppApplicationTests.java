6ee6c226883d6026516dd06aee694b5dfc0f9adb
Karina <rkaitimbetova@gmail.com>
2025-05-03 18:47:24 +0300

    SimpleWebApp

---
+++ src/test/java/com/example/simplevebapp/SimpleVebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplevebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleVebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
